5e8529f40222e0c0287cf71a641b56a27633f4d1
petergphillips <peter@greenthistle.co.uk>
2021-06-07 17:51:13 +0100

    DT-2122: ⬆️  Bump to latest versions (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
-  kotlin("plugin.spring") version "1.5.0"
-  kotlin("plugin.jpa") version "1.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
+  kotlin("plugin.spring") version "1.5.10"
+  kotlin("plugin.jpa") version "1.5.10"
   idea
 }
 
 configurations {
-  implementation { exclude(group = "tomcat-jdbc") }
+  implementation {
+    exclude(group = "tomcat-jdbc")
+    exclude(module = "spring-boot-graceful-shutdown")
+  }
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
@@ -21,29 +24,21 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.9.1")
+  runtimeOnly("org.flywaydb:flyway-core:7.9.2")
   runtimeOnly("org.postgresql:postgresql:42.2.20")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
-
-  implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1021"))
-  implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
-  testImplementation("org.testcontainers:localstack:1.15.3")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
