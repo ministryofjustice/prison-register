311e09e10f21ddf33cefca50d9f4a497d0aba5b9
Peter Phillips <peter@greenthistle.co.uk>
2021-08-26 12:04:27 +0100

    DT-2477: ⬆️ Upgrade to latest spring boot and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.6"
-  kotlin("plugin.spring") version "1.5.21"
-  kotlin("plugin.jpa") version "1.5.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.7"
+  kotlin("plugin.spring") version "1.5.30"
+  kotlin("plugin.jpa") version "1.5.30"
   idea
 }
 
