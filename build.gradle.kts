792718795bfab4741d684eaca44168b0694fdd3d
Peter Phillips <peter@greenthistle.co.uk>
2021-10-11 20:20:00 +0100

    DT-2721: ⬆️  Use latest gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.10-beta"
   kotlin("plugin.spring") version "1.5.31"
   kotlin("plugin.jpa") version "1.5.31"
   idea
