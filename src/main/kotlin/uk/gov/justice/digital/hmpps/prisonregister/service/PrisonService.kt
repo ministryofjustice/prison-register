2f4a99e61eacdaf9f6197a84987b3f6ed0f7cbf4
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-02-22 09:44:07 +0000

    HAAR-164 Add insert prison endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/PrisonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/PrisonService.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisonregister.service
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.microsoft.applicationinsights.TelemetryClient
-import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
@@ -15,10 +13,12 @@ import uk.gov.justice.digital.hmpps.prisonregister.model.PrisonRepository
 import uk.gov.justice.digital.hmpps.prisonregister.model.SetOutcome
 import uk.gov.justice.digital.hmpps.prisonregister.model.VideoLinkConferencingCentreRepository
 import uk.gov.justice.digital.hmpps.prisonregister.model.VideolinkConferencingCentre
+import uk.gov.justice.digital.hmpps.prisonregister.resource.GpDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.InsertPrisonDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.PrisonDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.UpdatePrisonDto
+import javax.persistence.EntityExistsException
 import javax.persistence.EntityNotFoundException
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.Size
 
 const val CLIENT_CAN_MAINTAIN_EMAIL_ADDRESSES = "hasRole('MAINTAIN_REF_DATA') and hasAuthority('SCOPE_write')"
 
@@ -30,17 +30,39 @@ class PrisonService(
   private val offenderManagementUnitRepository: OffenderManagementUnitRepository,
   private val telemetryClient: TelemetryClient
 ) {
-  fun findById(prisonId: String): Prison =
-    prisonRepository.findByIdOrNull(prisonId) ?: throw EntityNotFoundException("Prison $prisonId not found")
+  fun findById(prisonId: String): PrisonDto {
+    val prison = prisonRepository.findById(prisonId).orElseThrow { EntityNotFoundException("Prison $prisonId not found") }
+    return PrisonDto(prison)
+  }
 
-  fun findByGpPractice(gpPracticeCode: String): Prison =
-    prisonRepository.findByGpPracticeGpPracticeCode(gpPracticeCode)
+  fun findPrisonAndGpPracticeById(prisonId: String): GpDto {
+    val prison = prisonRepository.findById(prisonId).orElseThrow { EntityNotFoundException("Prison $prisonId not found") }
+    return GpDto(prison)
+  }
+
+  fun findByGpPractice(gpPracticeCode: String): GpDto {
+    val prison = prisonRepository.findByGpPracticeGpPracticeCode(gpPracticeCode)
       ?: throw EntityNotFoundException("Prison with gp practice $gpPracticeCode not found")
+    return GpDto(prison)
+  }
 
   fun findAll(): List<PrisonDto> = prisonRepository.findAll().map { PrisonDto(it) }
 
   fun findByActiveAndTextSearch(active: Boolean? = null, textSearch: String? = null): List<PrisonDto> =
     prisonRepository.findAll(PrisonFilter(active, textSearch)).map { PrisonDto(it) }
+
+  @Transactional
+  fun insertPrison(prisonInsertRecord: InsertPrisonDto): String {
+    if (prisonRepository.findById(prisonInsertRecord.prisonId).isPresent) {
+      throw EntityExistsException("Prison $prisonInsertRecord.prisonId already exists")
+    }
+
+    with(prisonInsertRecord) {
+      val prison = Prison(prisonId, prisonName, active)
+      telemetryClient.trackEvent("prison-register-insert", mapOf("prison" to prison.name), null)
+      return prisonRepository.save(prison).prisonId
+    }
+  }
 
   @Transactional
   fun updatePrison(prisonId: String, prisonUpdateRecord: UpdatePrisonDto): PrisonDto {
@@ -107,16 +129,3 @@ class PrisonService(
     }
   }
 }
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@Schema(description = "Prison Update Record")
-data class UpdatePrisonDto(
-  @Schema(description = "Name of the prison", example = "HMPPS Moorland", required = true) @field:Size(
-    max = 40,
-    min = 3,
-    message = "Prison name must be between 3 and 40 letters"
-  ) @field:NotBlank(message = "Prison name is required") val prisonName: String,
-
-  @Schema(description = "Whether the prison is still active", required = true) val active: Boolean
-
-)
