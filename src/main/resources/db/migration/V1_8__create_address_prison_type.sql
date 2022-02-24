2e7d22232770f1fd50f9b424fa15242d699ca1c9
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-02-24 08:43:08 +0000

    HAAR-165 Insert prison address data

--- src/main/resources/db/migration/V1_8__create_address_prison_type.sql
+++ src/main/resources/db/migration/V1_8__create_address_prison_type.sql
@@ -2,10 +2,10 @@ CREATE TABLE address
 (
     id              SERIAL      PRIMARY KEY,
     prison_id       VARCHAR(6)  NOT NULL,
-    address_line1   VARCHAR(80) NOT NULL,
+    address_line1   VARCHAR(80),
     address_line2   VARCHAR(80) NULL,
     town            VARCHAR(80) NULL,
-    county          VARCHAR(80) NOT NULL,
+    county          VARCHAR(80),
     postcode        VARCHAR(8)  NOT NULL,
     country         VARCHAR(16) NOT NULL,
     CONSTRAINT address_prison_fk FOREIGN KEY (prison_id) REFERENCES prison (prison_id)
