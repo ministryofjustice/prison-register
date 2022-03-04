782d0015073e673b641b25c724684f32fa18cbc7
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2022-03-04 14:34:12 +0000

    HAAR-211 Separate email endpoints into separate resource file and update swagger (#83)
    * HAAR-211 Separate email endpoints into separate resource file and update swagger
    
    * HAAR-211 Separate email endpoints into separate resource file and update swagger

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonResource.kt
@@ -9,31 +9,16 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonregister.model.Address
 import uk.gov.justice.digital.hmpps.prisonregister.model.Prison
-import uk.gov.justice.digital.hmpps.prisonregister.model.SetOutcome
 import uk.gov.justice.digital.hmpps.prisonregister.service.PrisonService
-import javax.validation.Valid
-import javax.validation.constraints.Email
 import javax.validation.constraints.Size
-
-const val OMU = "offender-management-unit"
-const val VCC = "videolink-conferencing-centre"
-const val EMAIL_ADDRESS = "email-address"
-const val PRISONS = "prisons"
-const val PRISON_BY_ID = "$PRISONS/id/{prisonId}"
-const val SECURE_PRISON_BY_ID = "secure/$PRISON_BY_ID"
 
 @RestController
 @Validated
@@ -61,7 +46,12 @@ class PrisonResource(private val prisonService: PrisonService) {
       ApiResponse(
         responseCode = "200",
         description = "Successful Operation",
-        content = [Content(mediaType = "application/json")]
+        content = [
+          Content(
+            mediaType = "application/json",
+            array = ArraySchema(schema = Schema(implementation = PrisonDto::class))
+          )
+        ]
       )
     ]
   )
@@ -87,208 +77,10 @@ class PrisonResource(private val prisonService: PrisonService) {
     @Parameter(description = "Active", example = "true", required = false) @RequestParam active: Boolean? = null,
     @Parameter(description = "Text search", example = "Sheffield", required = false) @RequestParam textSearch: String? = null
   ): List<PrisonDto> = prisonService.findByActiveAndTextSearch(active, textSearch)
-
-  @GetMapping(
-    "/$SECURE_PRISON_BY_ID/$VCC/$EMAIL_ADDRESS",
-    produces = [MediaType.TEXT_PLAIN_VALUE]
-  )
-  @Operation(summary = "Get a prison's Videolink Conferencing Centre email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "200",
-        description = "Returns the email address",
-        content = [Content(mediaType = MediaType.TEXT_PLAIN_VALUE)]
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId or similar"
-      ),
-      ApiResponse(
-        responseCode = "404",
-        description = "The prison does not have a Videolink Conferencing Centre email address"
-      ),
-    ]
-  )
-  fun getEmailForVideoConferencingCentre(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String,
-  ): ResponseEntity<String> =
-    prisonService
-      .getVccEmailAddress(prisonId)
-      ?.let { ResponseEntity.ok(it) }
-      ?: ResponseEntity.notFound().build()
-
-  @GetMapping(
-    "/$SECURE_PRISON_BY_ID/$OMU/$EMAIL_ADDRESS",
-    produces = [MediaType.TEXT_PLAIN_VALUE]
-  )
-  @Operation(summary = "Get a prison's Offender Management Unit email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "200",
-        description = "Returns the email address",
-        content = [Content(mediaType = MediaType.TEXT_PLAIN_VALUE)]
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId or similar"
-      ),
-      ApiResponse(
-        responseCode = "404",
-        description = "The prison does not have a Offender Management Unit email address"
-      ),
-    ]
-  )
-  fun getEmailForOffenderManagementUnit(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String,
-  ): ResponseEntity<String> =
-    prisonService
-      .getOmuEmailAddress(prisonId)
-      ?.let { ResponseEntity.ok(it) }
-      ?: ResponseEntity.notFound().build()
-
-  @PutMapping(
-    "/$SECURE_PRISON_BY_ID/$VCC/$EMAIL_ADDRESS",
-    consumes = [MediaType.TEXT_PLAIN_VALUE]
-  )
-  @Operation(summary = "Set or change a prison's Videolink Conferencing Centre email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "201",
-        description = "The email address was created"
-      ),
-      ApiResponse(
-        responseCode = "204",
-        description = "The email address was updated"
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId, email address or similar"
-      ),
-      ApiResponse(
-        responseCode = "404",
-        description = "No prison found for the supplied prison id"
-      ),
-    ]
-  )
-  fun putEmailAddressForVideolinkConferencingCentre(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String,
-
-    @Schema(description = "Email address", example = "a@b.com", required = true)
-    @RequestBody
-    @Valid
-    @Email
-    emailAddress: String
-  ): ResponseEntity<Void> =
-    when (prisonService.setVccEmailAddress(prisonId, emailAddress)) {
-      SetOutcome.CREATED -> ResponseEntity.status(HttpStatus.CREATED)
-      SetOutcome.UPDATED -> ResponseEntity.noContent()
-    }.build()
-
-  @PutMapping(
-    "/$SECURE_PRISON_BY_ID/$OMU/$EMAIL_ADDRESS",
-    consumes = [MediaType.TEXT_PLAIN_VALUE]
-  )
-  @Operation(summary = "Set or change a prison's Offender Management Unit email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "201",
-        description = "The email address was created"
-      ),
-      ApiResponse(
-        responseCode = "204",
-        description = "The email address was updated"
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId, email address, media type or similar"
-      ),
-      ApiResponse(
-        responseCode = "404",
-        description = "No prison found for the supplied prison id"
-      ),
-    ]
-  )
-  fun putEmailAddressForOffenderManagementUnit(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String,
-
-    @Schema(description = "Email address", example = "a@b.com", required = true)
-    @RequestBody
-    @Valid
-    @Email
-    emailAddress: String
-  ): ResponseEntity<Void> =
-    when (prisonService.setOmuEmailAddress(prisonId, emailAddress)) {
-      SetOutcome.CREATED -> ResponseEntity.status(HttpStatus.CREATED)
-      SetOutcome.UPDATED -> ResponseEntity.noContent()
-    }.build()
-
-  @DeleteMapping("/$SECURE_PRISON_BY_ID/$VCC/$EMAIL_ADDRESS")
-  @Operation(summary = "Remove a prison's Videolink Conferencing Centre email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "204",
-        description = "The email address was removed"
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId or similar"
-      ),
-    ]
-  )
-  fun deleteEmailAddressForVideolinkConferencingCentre(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String
-  ): ResponseEntity<Void> {
-    prisonService.deleteVccEmailAddress(prisonId)
-    return ResponseEntity.noContent().build()
-  }
-
-  @DeleteMapping("/$SECURE_PRISON_BY_ID/$OMU/$EMAIL_ADDRESS")
-  @Operation(summary = "Remove a prison's Offender Management Unit email address")
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "204",
-        description = "The email address was removed"
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Client error - invalid prisonId or similar"
-      ),
-    ]
-  )
-  fun deleteEmailAddressForOffenderManagementUnit(
-    @Schema(description = "Prison ID", example = "MDI", required = true)
-    @PathVariable
-    @Size(max = 12, min = 2)
-    prisonId: String
-  ): ResponseEntity<Void> {
-    prisonService.deleteOmuEmailAddress(prisonId)
-    return ResponseEntity.noContent().build()
-  }
 }
 
 @JsonInclude(NON_NULL)
-@Schema(description = "Prison Information", example = "Details about a prison")
+@Schema(description = "Prison Information")
 data class PrisonDto(
   @Schema(description = "Prison ID", example = "MDI", required = true) val prisonId: String,
   @Schema(description = "Name of the prison", example = "Moorland HMP", required = true) val prisonName: String,
@@ -301,15 +93,16 @@ data class PrisonDto(
 }
 
 data class AddressDto(
+  @Schema(description = "Unique ID of the address", example = "10000", required = true) val id: Long,
   @Schema(description = "Address line 1", example = "Bawtry Road", required = false) val addressLine1: String?,
   @Schema(description = "Address line 2", example = "Hatfield Woodhouse", required = false) val addressLine2: String?,
   @Schema(description = "Village/Town/City", example = "Doncaster", required = true) val town: String,
   @Schema(description = "County", example = "South Yorkshire", required = false) val county: String?,
-  @Schema(description = "Country", example = "England", required = true) val country: String,
   @Schema(description = "Postcode", example = "DN7 6BW", required = true) val postcode: String,
+  @Schema(description = "Country", example = "England", required = true) val country: String,
 
 ) {
   constructor(address: Address) : this(
-    address.addressLine1, address.addressLine2, address.town, address.county, address.country, address.postcode
+    address.id!!, address.addressLine1, address.addressLine2, address.town, address.county, address.postcode, address.country
   )
 }
