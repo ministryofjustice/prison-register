aff169a6b66acdc7696f3c06705c76d9b6dd606a
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-03-16 12:25:38 +0000

    HAAR-248 gender always returned

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonResource.kt
@@ -105,8 +105,8 @@ data class PrisonDto(
   @Schema(description = "Prison ID", example = "MDI", required = true) val prisonId: String,
   @Schema(description = "Name of the prison", example = "Moorland HMP", required = true) val prisonName: String,
   @Schema(description = "Whether the prison is still active", required = true) val active: Boolean,
-  @Schema(description = "Whether the prison has male prisoners") val male: Boolean? = null,
-  @Schema(description = "Whether the prison has female prisoners") val female: Boolean? = null,
+  @Schema(description = "Whether the prison has male prisoners") val male: Boolean,
+  @Schema(description = "Whether the prison has female prisoners") val female: Boolean,
   @Schema(description = "List of types for this prison") val types: List<PrisonTypeDto> = listOf(),
   @Schema(description = "List of address for this prison") val addresses: List<AddressDto> = listOf(),
 ) {
