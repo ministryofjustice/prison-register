9cd32e4d969955e3722d5e6e82b874e84904193b
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-05-04 11:25:50 +0100

    HAAR-339: Added Parc (PYI) as contracted prison

--- src/main/resources/db/migration/V1_15__add_prison_contracted_flag.sql
+++ src/main/resources/db/migration/V1_15__add_prison_contracted_flag.sql
@@ -1,3 +1,3 @@
 ALTER TABLE prison ADD COLUMN contracted BOOLEAN NOT NULL DEFAULT false;
 
-UPDATE prison SET contracted = true WHERE prison_id IN ('ACI', 'ASI', 'BZI', 'DNI', 'DGI', 'FWI', 'FBI', 'LGI', 'NLI', 'OWI', 'PBI', 'PFI', 'PRI', 'RHI', 'TSI');
+UPDATE prison SET contracted = true WHERE prison_id IN ('ACI', 'ASI', 'BZI', 'DNI', 'DGI', 'FWI', 'FBI', 'LGI', 'NLI', 'OWI', 'PBI', 'PFI', 'PRI', 'PYI', 'RHI', 'TSI');
