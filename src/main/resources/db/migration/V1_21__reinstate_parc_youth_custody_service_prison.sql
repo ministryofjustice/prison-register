572b6c892706acd19d4fd61a5243cac56aab9728
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-05-17 16:53:43 +0100

    HAAR-455: flyway script to re-instate Parc Youth Custody Service facility

---
+++ src/main/resources/db/migration/V1_21__reinstate_parc_youth_custody_service_prison.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRISON(prison_id, name, active, male, female, contracted) VALUES ('PYI', 'Parc (HMYOI)', true, true, false, true);
+INSERT INTO PRISON_TYPE(PRISON_ID, TYPE) VALUES ('PYI', 'YCS');
+INSERT INTO ADDRESS(prison_id, address_line1, town, postcode, country) VALUES ('PYI', 'Heol Hopcyn John', 'Bridgend', 'CF35 6AP', 'Wales');
