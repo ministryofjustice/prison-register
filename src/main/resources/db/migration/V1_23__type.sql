925d3014ac235486d511972013782d38d29660ae
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-05-19 16:13:30 +0100

    HAAR-444 Add table of prison types to ensure data integrity

---
+++ src/main/resources/db/migration/V1_23__type.sql
@@ -0,0 +1,14 @@
+CREATE TABLE type
+(
+    id          VARCHAR(6)  NOT NULL PRIMARY KEY,
+    description VARCHAR(80) NOT NULL
+);
+
+INSERT INTO type
+VALUES ('HMP', 'Her Majesty’s Prison (HMP)'),
+       ('YOI', 'Her Majesty’s Youth Offender Institution (YOI)'),
+       ('IRC', 'Immigration Removal Centre (IRC)'),
+       ('STC', 'Secure Training Centre (STC)'),
+       ('YCS', 'Youth Custody Service (YCS)');
+
+ALTER TABLE prison_type ADD CONSTRAINT prison_type_type_fk FOREIGN KEY(TYPE) REFERENCES type(id);
