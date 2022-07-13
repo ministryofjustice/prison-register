9ed627ac830c91c196c549fb0b03317043477246
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-07-13 15:34:35 +0100

    HAAR-663: remove un-needed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.6")
-  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
   runtimeOnly("com.h2database:h2:2.1.214")
   runtimeOnly("org.flywaydb:flyway-core:8.5.13")
