a59066a01f49765616537ebb396848c774d9310c
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-09-05 17:28:57 +0100

    HAAR-753: ⬆️ upgrade gradle-spring-boot plugin to 4.4.3-beta (#146)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.2-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3-beta"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   idea
