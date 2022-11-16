9f36febd16441c7375fc3c8bda82d160e379f61a
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-11-16 11:28:11 +0000

    HAAR-994: ⬆️ upgrade gradle spring boot plugin to 4.6.0-beta (#159)
    * HAAR-994: ⬆️ upgrade gradle spring boot plugin to 4.6.0-beta
    
    * HAAR-994: ⬆️ upgrade gradle spring boot plugin to 4.6.0-beta
    
    * HAAR-994: ⬆️ upgrade gradle spring boot plugin to 4.6.0-beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7"
-  kotlin("plugin.spring") version "1.7.20"
-  kotlin("plugin.jpa") version "1.7.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.6.0-beta"
+  kotlin("plugin.spring") version "1.7.21"
+  kotlin("plugin.jpa") version "1.7.21"
   idea
 }
 
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.12")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
 
   runtimeOnly("com.h2database:h2:2.1.214")
   runtimeOnly("org.flywaydb:flyway-core")
@@ -32,14 +32,14 @@ dependencies {
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.code.gson:gson:2.10")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.0")
 
   implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.12")
   implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.333"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.343"))
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
@@ -47,7 +47,7 @@ dependencies {
   testImplementation("org.testcontainers:localstack:1.17.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-  testImplementation("org.mockito:mockito-inline:4.8.1")
+  testImplementation("org.mockito:mockito-inline:4.9.0")
   testImplementation("org.springframework.security:spring-security-test")
 }
 
