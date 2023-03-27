60bed22f3a96a3be0fa44d89a1697586a3b3db72
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-03-27 11:15:52 +0100

    HAAR-1296: ⬆️ update to spring-boot-plugin-4.8.5-beta-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.5-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.5-beta-2"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
   idea
@@ -39,7 +39,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.15")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.15")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.434"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.435"))
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.37.0")
