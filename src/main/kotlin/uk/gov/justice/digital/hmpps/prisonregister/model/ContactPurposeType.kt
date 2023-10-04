4c714f7b2c6cbadb7521980954f925df92ebc6c6
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-10-04 13:56:43 +0100

    VB-2729 Establishment specific contact information maintained in prison-register from admin, adding new email address

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/model/ContactPurposeType.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.prisonregister.model
+
+import uk.gov.justice.digital.hmpps.prisonregister.exceptions.BadContactPurposeTypeException
+
+enum class ContactPurposeType(val value: String) {
+  SOCIAL_VISIT("social-visit"),
+  VIDEO_LINK_CONFERENCING("video-link-conferencing"),
+  OFFENDER_MANAGEMENT_UNIT("offender-management-unit"),
+  ;
+
+  companion object {
+    fun getFromPathVariable(pathVariable: String): ContactPurposeType {
+      return entries.firstOrNull { it.value == pathVariable } ?: throw BadContactPurposeTypeException(pathVariable)
+    }
+  }
+}
