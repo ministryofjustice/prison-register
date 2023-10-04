a651abe18472cd3a382267ba9a2dfcdfb141b102
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-10-04 15:14:42 +0100

    VB-2729 Establishment specific contact information maintained in prison-register from admin, adding new email address, trying to kick off fly way test in circle ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,50 +5,58 @@ plugins {
   idea
 }
 
+springBoot {
+  mainClass.value("uk.gov.justice.digital.hmpps.prisonregister.PrisonRegisterApplicationkt")
+}
+
 configurations {
-  implementation {
-    exclude(group = "tomcat-jdbc")
-    exclude(module = "spring-boot-graceful-shutdown")
-  }
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
 dependencies {
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-  runtimeOnly("org.postgresql:postgresql:42.6.0")
-  runtimeOnly("org.flywaydb:flyway-core")
+  implementation("com.google.guava:guava:32.1.2-jre")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.1")
 
-  runtimeOnly("com.h2database:h2:2.1.214")
-  runtimeOnly("org.flywaydb:flyway-core")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.30.0")
+
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+  implementation("org.springframework.data:spring-data-commons:3.1.3")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
+
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.7.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.7.0")
+  implementation("org.apache.commons:commons-csv:1.10.0")
+  implementation("org.freemarker:freemarker:2.3.32")
+
   runtimeOnly("org.postgresql:postgresql:42.6.0")
+  runtimeOnly("org.flywaydb:flyway-core")
 
-  implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
-  implementation("com.google.code.gson:gson:2.10.1")
-  implementation("com.google.guava:guava:32.1.2-jre")
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.1")
-
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.520"))
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.0.0")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-  testImplementation("org.testcontainers:localstack:1.18.1")
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("org.testcontainers:localstack:1.18.3")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("org.testcontainers:postgresql:1.19.0")
+  testImplementation("org.testcontainers:localstack:1.19.0")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.550")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
 }
 
 java {
