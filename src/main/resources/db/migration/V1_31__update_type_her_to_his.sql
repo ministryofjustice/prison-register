4a9e2eff428b11d0e3dca90a8a3ab12f6ae4bc88
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-02-21 19:07:11 +0000

    MAP-812 Fix flyway, change Her to His

--- src/main/resources/db/migration/V1_31__update_type_her_to_his.sql
+++ src/main/resources/db/migration/V1_31__update_type_her_to_his.sql
@@ -1,2 +1,2 @@
-UPDATE TABLE type SET description = 'His Majesty’s Prison (HMP)' WHERE id = 'HMP';
-UPDATE TABLE type SET description = 'His Majesty’s Youth Offender Institution (YOI)' WHERE id = 'YOI';
+UPDATE type SET description = 'His Majesty’s Prison (HMP)' WHERE id = 'HMP';
+UPDATE type SET description = 'His Majesty’s Youth Offender Institution (YOI)' WHERE id = 'YOI';
