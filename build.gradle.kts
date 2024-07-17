008c80d29b3453713f9a860d9724b6975e00764a
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-07-17 10:40:40 +0100

    library upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
   kotlin("plugin.spring") version "1.9.22"
@@ -15,8 +17,8 @@ configurations {
 
 dependencies {
   implementation("com.google.guava:guava:32.1.2-jre")
-  implementation("commons-validator:commons-validator:1.7")
-  implementation("com.googlecode.libphonenumber:libphonenumber:8.13.12")
+  implementation("commons-validator:commons-validator:1.9.0")
+  implementation("com.googlecode.libphonenumber:libphonenumber:8.13.41")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
@@ -24,42 +26,42 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
   implementation("com.jayway.jsonpath:json-path:2.9.0")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.30.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.33.4")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-  implementation("org.springframework.data:spring-data-commons:3.2.3")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
-  implementation("io.swagger:swagger-annotations:1.6.11")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
+  implementation("org.springframework.data:spring-data-commons:3.3.2")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.6.0")
+  implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("net.javacrumbs.shedlock:shedlock-spring:5.7.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.7.0")
-  implementation("org.apache.commons:commons-csv:1.10.0")
-  implementation("org.freemarker:freemarker:2.3.32")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.13.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.13.0")
+  implementation("org.apache.commons:commons-csv:1.11.0")
+  implementation("org.freemarker:freemarker:2.3.33")
 
-  runtimeOnly("org.postgresql:postgresql:42.7.2")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16")
-  testImplementation("org.wiremock:wiremock-standalone:3.4.1")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
+  testImplementation("org.wiremock:wiremock-standalone:3.8.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.18.3")
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+  testImplementation("org.testcontainers:localstack:1.19.8")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
+  testImplementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  testImplementation("io.jsonwebtoken:jjwt:0.12.6")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.19.5")
-  testImplementation("org.testcontainers:localstack:1.19.5")
-  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.662")
-  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
+  testImplementation("org.testcontainers:postgresql:1.19.8")
+  testImplementation("org.testcontainers:localstack:1.19.8")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.761")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
 }
 
 java {
@@ -70,9 +72,7 @@ repositories {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "21"
-    }
+  withType<KotlinCompile> {
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
   }
 }
