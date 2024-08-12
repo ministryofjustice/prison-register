25a55df7214929c17e4b30bc4a2547ff2e7d4060
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-08-12 12:42:47 +0100

    Update V1_33__update_LGI_data.sql

--- src/main/resources/db/migration/V1_33__update_LGI_data.sql
+++ src/main/resources/db/migration/V1_33__update_LGI_data.sql
@@ -1,2 +1,2 @@
 UPDATE PRISON SET contracted = false where PRISON_ID = 'LGI';
-UPDATE prison_operator SET  operator_id = 1 where PRISON_ID = 'LGI';
+UPDATE prison_operator SET operator_id = 1 where PRISON_ID = 'LGI';
