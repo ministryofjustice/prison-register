d6eeaf43bdc13222efcd122f513aa8832df26b0a
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-09-17 16:59:53 +0100

    MAP-1458 PrisonEmailResource GET scenarios

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonEmailResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/PrisonEmailResourceTest.kt
@@ -7,6 +7,7 @@ import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.http.HttpStatus
 import uk.gov.justice.digital.hmpps.prisonregister.model.DepartmentType.OFFENDER_MANAGEMENT_UNIT
+import uk.gov.justice.digital.hmpps.prisonregister.model.DepartmentType.VIDEOLINK_CONFERENCING_CENTRE
 import uk.gov.justice.digital.hmpps.prisonregister.model.SetOutcome
 import uk.gov.justice.digital.hmpps.prisonregister.service.PrisonService
 
@@ -16,6 +17,8 @@ import uk.gov.justice.digital.hmpps.prisonregister.service.PrisonService
 class PrisonEmailResourceTest {
   private val prisonService: PrisonService = mock()
   private val prisonEmailResource = PrisonEmailResource(prisonService)
+  private val prisonId = "MDI"
+  val expectedEmail = "email@example.com"
 
   @Test
   fun `put email - create`() {
@@ -31,5 +34,32 @@ class PrisonEmailResourceTest {
     val response = prisonEmailResource.putEmailAddressForOffenderManagementUnit("MDI", "a@b.com")
     assertThat(response.statusCode).isEqualTo(HttpStatus.NO_CONTENT)
     verify(prisonService).setEmailAddress("MDI", "a@b.com", OFFENDER_MANAGEMENT_UNIT)
+  }
+
+  @Test
+  fun `get email - not found when email does not exist Videolink conferencing centre`() {
+    whenever(prisonService.getEmailAddress(prisonId, VIDEOLINK_CONFERENCING_CENTRE))
+      .thenReturn(null)
+    val response = prisonEmailResource.getEmailForVideoConferencingCentre(prisonId)
+    assertThat(response.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+    verify(prisonService).getEmailAddress(prisonId, VIDEOLINK_CONFERENCING_CENTRE)
+  }
+
+  @Test
+  fun `get email - Videolink conferencing centre`() {
+    whenever(prisonService.getEmailAddress(prisonId, VIDEOLINK_CONFERENCING_CENTRE))
+      .thenReturn(expectedEmail)
+    val response = prisonEmailResource.getEmailForVideoConferencingCentre(prisonId)
+    assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    verify(prisonService).getEmailAddress(prisonId, VIDEOLINK_CONFERENCING_CENTRE)
+  }
+
+  @Test
+  fun `get email - not found when email does not exist Offender management unit`() {
+    whenever(prisonService.getEmailAddress(prisonId, OFFENDER_MANAGEMENT_UNIT))
+      .thenReturn(null)
+    val response = prisonEmailResource.getEmailForOffenderManagementUnit(prisonId)
+    assertThat(response.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+    verify(prisonService).getEmailAddress(prisonId, OFFENDER_MANAGEMENT_UNIT)
   }
 }
