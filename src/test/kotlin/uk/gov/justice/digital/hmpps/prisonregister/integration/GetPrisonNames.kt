3b9c7d139f05cf98979ce7362cd8a2d45dcdbe79
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-10-15 11:43:44 +0100

    MAP-1683 add active parameter test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/integration/GetPrisonNames.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/integration/GetPrisonNames.kt
@@ -30,6 +30,30 @@ class GetPrisonNames : IntegrationTest() {
     }
   }
 
+  @Test
+  fun `should return prison names active only and ids by name order`() {
+    // Given
+    val endPoint = "/prisons/names?active=true"
+
+    // When
+    val responseSpec = webTestClient.get().uri(endPoint)
+      .exchange()
+
+    // Then
+    responseSpec.expectStatus().isOk
+
+    val prisonNames = getPrisonNames(responseSpec.expectBody())
+    Assertions.assertThat(prisonNames).isNotEmpty
+    with(prisonNames[0]) {
+      Assertions.assertThat(prisonId).isEqualTo("ACI")
+      Assertions.assertThat(prisonName).isEqualTo("Altcourse (HMP & YOI)")
+    }
+    with(prisonNames.last()) {
+      Assertions.assertThat(prisonId).isEqualTo("WMI")
+      Assertions.assertThat(prisonName).isEqualTo("Wymott (HMP & YOI)")
+    }
+  }
+
   fun getPrisonNames(returnResult: WebTestClient.BodyContentSpec): Array<PrisonNameDto> {
     return objectMapper.readValue(returnResult.returnResult().responseBody, Array<PrisonNameDto>::class.java)
   }
