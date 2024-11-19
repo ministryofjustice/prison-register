6ea1c607088af86d8f383b40359d09b8fa3f6996
Julien Nioche <julien.nioche@digital.justice.gov.uk>
2024-11-19 14:48:01 +0000

    MAP 1842 Fix Netty security issue (#296)
    * Force version of netty common to fix security issue + update sqs boot starter
    
    Signed-off-by: Julien Nioche <julien.nioche@digital.justice.gov.uk>
    
    * Netty fixed from upstream
    
    Signed-off-by: Julien Nioche <julien.nioche@digital.justice.gov.uk>
    
    ---------
    
    Signed-off-by: Julien Nioche <julien.nioche@digital.justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import uk.gov.justice.digital.hmpps.gradle.PortForwardRedisTask
 import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   id("org.jetbrains.kotlinx.kover") version "0.8.3"
