4ffbe761a64822249f8e9dc619bdc4ead4469d3b
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2025-02-03 13:43:53 +0000

    MAP-2047 remove white space before Cymru (#313)
    * MAP-2047 remove white space before Cymru
    
    * MAP-2047 put change on one lin

---
+++ src/main/resources/db/migration/V1_39__remove_whitespace_in_address_table.sql
@@ -0,0 +1,7 @@
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'UKI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'BWI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'CFI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'UKI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'PRI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'PYI';
+UPDATE address SET country_in_welsh = 'Cymru' WHERE prison_id = 'SWI';
