ff8b1d94fe9ba51cda51853bfb0cab197abe7181
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2025-02-04 13:49:32 +0000

    MAP-2041 seperate endpoint to update welsh address

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/PrisonAddressServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/PrisonAddressServiceTest.kt
@@ -322,6 +322,11 @@ class PrisonAddressServiceTest {
           "county" to county,
           "postcode" to postcode,
           "country" to country,
+          "addressLine1InWelsh" to null,
+          "addressLine2InWelsh" to null,
+          "townInWelsh" to null,
+          "countyInWelsh" to null,
+          "countryInWelsh" to null,
         )
 
         val actualAddress = prisonAddressService.addAddress(prison.prisonId, additionalAddress)
