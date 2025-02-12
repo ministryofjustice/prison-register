3e0c81e2eca3cf7b9511afee7bb47b4baf3ee1f7
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2025-02-12 13:50:46 +0000

    Map 2047 prison register api correct welsh address (#317)
    * MAP-2027 corrected welsh addresses
    
    * MAP-2047 apply correct welsh addresses
    
    were incorrect in previous migration

---
+++ src/main/resources/db/migration/V1_40__correct_welsh_prison_addresses.sql
@@ -0,0 +1,19 @@
+UPDATE address SET
+address_line1_in_welsh = 'Heol Hopcyn John',
+town_in_welsh = 'Coety, Pen-y-bont',
+country_in_welsh = 'Cymru'
+WHERE prison_id = 'PYI';
+
+UPDATE address SET
+address_line1_in_welsh = 'Coed-y-Paen',
+town_in_welsh = 'Pont-y-pŵl',
+county_in_welsh = 'Sir Fynwy',
+country_in_welsh = 'Cymru'
+WHERE prison_id = 'UPI';
+
+UPDATE address SET
+address_line1_in_welsh = '200 Heol Ystumllwynarth',
+town_in_welsh = 'Abertawe',
+country_in_welsh = 'Cymru'
+WHERE prison_id = 'SWI';
+
