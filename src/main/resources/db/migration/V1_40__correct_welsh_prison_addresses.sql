dbb1931c0ed91b22b8c42dbfb84aaab6ebc88bf7
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-02-12 13:15:54 +0000

    MAP-2027 corrected welsh addresses

---
+++ src/main/resources/db/migration/V1_40__correct_welsh_prison_addresses.sql
@@ -0,0 +1,19 @@
+UPDATE address SET
+address_line1_in_welsh = '47 Maryport Street',
+town_in_welsh = 'Brynbuga',
+county_in_welsh = 'Sir Fynwy',
+country_in_welsh = ' Cymru'
+WHERE prison_id = 'UKI';
+
+UPDATE address SET
+address_line1_in_welsh = 'Heol Hopcyn John',
+town_in_welsh = 'Coety, Pen-y-bont',
+country_in_welsh = ' Cymru'
+WHERE prison_id = 'PYI';
+
+UPDATE address SET
+address_line1_in_welsh = '200 Heol Ystumllwynarth',
+town_in_welsh = 'Abertawe',
+country_in_welsh = ' Cymru'
+WHERE prison_id = 'SWI';
+
