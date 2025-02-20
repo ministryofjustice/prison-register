9d1657daddbe3d7f5fa53d787dde9effbb8db437
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-02-20 11:33:33 +0000

    more minor deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
   kotlin("plugin.spring") version "2.1.10"
   kotlin("plugin.jpa") version "2.1.10"
-  id("org.jetbrains.kotlinx.kover") version "0.9.0"
+  id("org.jetbrains.kotlinx.kover") version "0.9.1"
   idea
 }
 
@@ -22,7 +22,7 @@ configurations {
 dependencies {
   implementation("com.google.guava:guava:33.4.0-jre")
   implementation("commons-validator:commons-validator:1.9.0")
-  implementation("com.googlecode.libphonenumber:libphonenumber:8.13.52")
+  implementation("com.googlecode.libphonenumber:libphonenumber:8.13.55")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
@@ -33,21 +33,21 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.10.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.13.1")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
-  implementation("org.springframework.data:spring-data-commons:3.4.1")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.7.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.7.0")
-  implementation("io.swagger.core.v3:swagger-annotations:2.2.27")
+  implementation("org.springframework.data:spring-data-commons:3.4.3")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.8.5")
+  implementation("io.swagger.core.v3:swagger-annotations:2.2.28")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("net.javacrumbs.shedlock:shedlock-spring:5.16.0")
   implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.16.0")
-  implementation("org.apache.commons:commons-csv:1.12.0")
+  implementation("org.apache.commons:commons-csv:1.13.0")
   implementation("org.apache.commons:commons-compress:1.27.1") // Address CVE-2024-25710 and CVE-2024-26308 present in v1.24
-  implementation("org.freemarker:freemarker:2.3.33")
+  implementation("org.freemarker:freemarker:2.3.34")
   implementation("io.netty:netty-common:4.1.118.Final")
 
   runtimeOnly("org.postgresql:postgresql")
@@ -56,17 +56,17 @@ dependencies {
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24")
-  testImplementation("org.wiremock:wiremock-standalone:3.11.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25")
+  testImplementation("org.wiremock:wiremock-standalone:3.12.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.jsonwebtoken:jjwt:0.12.6")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.20.4")
-  testImplementation("org.testcontainers:localstack:1.20.4")
-  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.780")
+  testImplementation("org.testcontainers:postgresql:1.20.5")
+  testImplementation("org.testcontainers:localstack:1.20.5")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.782")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
 }
 
