675f655bead95b0bbfb491952584e8c5ffcd570c
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2026-04-01 11:50:26 +0100

    exclude nulls from prison names endpoint (#368)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/dto/PrisonNameDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/dto/PrisonNameDto.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.hmpps.prisonregister.resource.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Full name of prison with id")
+@JsonInclude(NON_NULL)
 class PrisonNameDto(
   @Schema(description = "Prison ID", example = "MDI", required = true)
   val prisonId: String,
