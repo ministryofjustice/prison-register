809ad8eead191ff89213d9f5bd92d92e50e89233
Andy Marke <andy.marke@justice.gov.uk>
2026-08-05 14:12:51 +0100

    SDIT-3937 Add Court sync for update courts (#416)
    TODO
    * Handle updating addresses etc
    * Testing addresses, phones and email address

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/LegacySyncResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonregister/resource/LegacySyncResourceIntTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.prisonregister.resource
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,7 +11,9 @@ import uk.gov.justice.digital.hmpps.prisonregister.ErrorResponse
 import uk.gov.justice.digital.hmpps.prisonregister.dsl.Root
 import uk.gov.justice.digital.hmpps.prisonregister.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonregister.integration.expectBodyResponse
+import uk.gov.justice.digital.hmpps.prisonregister.model.Court
 import uk.gov.justice.digital.hmpps.prisonregister.model.CourtRepository
+import java.time.LocalDate
 
 class LegacySyncResourceIntTest : IntegrationTestBase() {
   @Autowired
@@ -79,90 +82,10 @@ class LegacySyncResourceIntTest : IntegrationTestBase() {
   }
 
   @Nested
-  inner class Court {
-    val courtRequest = LegacyAgencyDto(
-      agencyType = LegacyAgencyType.COURT,
-      name = "Sheffield MC",
-      description = "Sheffield Magistrates' Court",
-      active = true,
-      inactiveDate = null,
-      cjitCode = "123456789",
-      areaCode = "52",
-      regionCode = "YOHUM",
-      geographicalAreaCode = null,
-      payrollRegionCode = null,
-      courtTypeCode = "MC",
-      addresses = listOf(
-        LegacyAgencyAddressDto(
-          addressLine1 = "Castle Street",
-          addressLine2 = null,
-          town = "Sheffield",
-          county = "South Yorkshire",
-          postcode = "S3 8LU",
-          country = "England",
-        ),
-      ),
-      emailAddresses = listOf(LegacyAgencyEmailDto(address = "test.sheffield.mc@justice.gov.uk")),
-      phoneNumbers = listOf(LegacyAgencyPhoneDto(number = "0114 555 5555")),
-    )
+  inner class WhenCourt {
 
     @Nested
-    inner class Validation {
-      @Test
-      fun `area code is not valid`() {
-        val errorResponse: ErrorResponse = webTestClient.post()
-          .uri("/sync/agency/id/{agencyId}", "SHEFMC")
-          .accept(MediaType.APPLICATION_JSON)
-          .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
-          .bodyValue(courtRequest.copy(areaCode = "ZZZ"))
-          .exchange()
-          .expectStatus().isBadRequest.expectBodyResponse()
-
-        assertThat(errorResponse.developerMessage).isEqualTo("ZZZ area code not found for agency SHEFMC")
-      }
-
-      @Test
-      fun `region code is not valid`() {
-        val errorResponse: ErrorResponse = webTestClient.post()
-          .uri("/sync/agency/id/{agencyId}", "SHEFMC")
-          .accept(MediaType.APPLICATION_JSON)
-          .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
-          .bodyValue(courtRequest.copy(regionCode = "ZZZ"))
-          .exchange()
-          .expectStatus().isBadRequest.expectBodyResponse()
-
-        assertThat(errorResponse.developerMessage).isEqualTo("ZZZ region code not found for agency SHEFMC")
-      }
-
-      @Test
-      fun `court type code is not valid`() {
-        val errorResponse: ErrorResponse = webTestClient.post()
-          .uri("/sync/agency/id/{agencyId}", "SHEFMC")
-          .accept(MediaType.APPLICATION_JSON)
-          .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
-          .bodyValue(courtRequest.copy(courtTypeCode = "ZZZ"))
-          .exchange()
-          .expectStatus().isBadRequest.expectBodyResponse()
-
-        assertThat(errorResponse.developerMessage).isEqualTo("ZZZ court type not found for agency SHEFMC")
-      }
-
-      @Test
-      fun `court type code missing`() {
-        val errorResponse: ErrorResponse = webTestClient.post()
-          .uri("/sync/agency/id/{agencyId}", "SHEFMC")
-          .accept(MediaType.APPLICATION_JSON)
-          .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
-          .bodyValue(courtRequest.copy(courtTypeCode = null))
-          .exchange()
-          .expectStatus().isBadRequest.expectBodyResponse()
-
-        assertThat(errorResponse.developerMessage).isEqualTo("null court type not found for agency SHEFMC")
-      }
-    }
-
-    @Nested
-    inner class NewCourtHappyPath {
+    inner class Create {
       val courtRequest = LegacyAgencyDto(
         agencyType = LegacyAgencyType.COURT,
         name = "Sheffield MC",
@@ -189,29 +112,234 @@ class LegacySyncResourceIntTest : IntegrationTestBase() {
         phoneNumbers = listOf(LegacyAgencyPhoneDto(number = "0114 555 5555")),
       )
 
-      @Test
-      fun `will create the core court data`() {
-        val response: LegacyAgencyResponse = webTestClient.post()
-          .uri("/sync/agency/id/{agencyId}", "SHEFMC")
-          .accept(MediaType.APPLICATION_JSON)
-          .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
-          .bodyValue(courtRequest.copy(addresses = emptyList(), emailAddresses = emptyList(), phoneNumbers = emptyList()))
-          .exchange()
-          .expectStatus().isOk.expectBodyResponse()
+      @Nested
+      inner class Validation {
+        @Test
+        fun `area code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(areaCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
 
-        assertThat(response.updated).isFalse
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ area code not found for agency SHEFMC")
+        }
 
-        val court = courtRepository.findByCourtId("SHEFMC")
+        @Test
+        fun `region code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(regionCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
 
-        with(court) {
-          assertThat(name).isEqualTo("Sheffield MC")
-          assertThat(description).isEqualTo("Sheffield Magistrates' Court")
-          assertThat(active).isTrue
-          assertThat(inactiveDate).isNull()
-          assertThat(cjitCode).isEqualTo("123456789")
-          assertThat(area?.description).isEqualTo("South Yorkshire")
-          assertThat(region?.description).isEqualTo("Yorkshire & Humberside")
-          assertThat(courtType.description).isEqualTo("Magistrates Court")
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ region code not found for agency SHEFMC")
+        }
+
+        @Test
+        fun `court type code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(courtTypeCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ court type not found for agency SHEFMC")
+        }
+
+        @Test
+        fun `court type code missing`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(courtTypeCode = null))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("null court type not found for agency SHEFMC")
+        }
+      }
+
+      @Nested
+      inner class HappyPath {
+
+        @Test
+        fun `will create the core court data`() {
+          val response: LegacyAgencyResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(addresses = emptyList(), emailAddresses = emptyList(), phoneNumbers = emptyList()))
+            .exchange()
+            .expectStatus().isOk.expectBodyResponse()
+
+          assertThat(response.updated).isFalse
+
+          val court = courtRepository.findByCourtId("SHEFMC")
+
+          with(court) {
+            assertThat(name).isEqualTo("Sheffield MC")
+            assertThat(description).isEqualTo("Sheffield Magistrates' Court")
+            assertThat(active).isTrue
+            assertThat(inactiveDate).isNull()
+            assertThat(cjitCode).isEqualTo("123456789")
+            assertThat(area?.description).isEqualTo("South Yorkshire")
+            assertThat(region?.description).isEqualTo("Yorkshire & Humberside")
+            assertThat(courtType.description).isEqualTo("Magistrates Court")
+          }
+        }
+      }
+    }
+
+    @Nested
+    inner class Update {
+      val courtRequest = LegacyAgencyDto(
+        agencyType = LegacyAgencyType.COURT,
+        name = "Sheffield MC",
+        description = "Sheffield Magistrates' Court",
+        active = true,
+        inactiveDate = null,
+        cjitCode = "123456789",
+        areaCode = "52",
+        regionCode = "YOHUM",
+        geographicalAreaCode = null,
+        payrollRegionCode = null,
+        courtTypeCode = "MC",
+        addresses = listOf(
+          LegacyAgencyAddressDto(
+            addressLine1 = "Castle Street",
+            addressLine2 = null,
+            town = "Sheffield",
+            county = "South Yorkshire",
+            postcode = "S3 8LU",
+            country = "England",
+          ),
+        ),
+        emailAddresses = listOf(LegacyAgencyEmailDto(address = "test.sheffield.mc@justice.gov.uk")),
+        phoneNumbers = listOf(LegacyAgencyPhoneDto(number = "0114 555 5555")),
+      )
+
+      lateinit var court: Court
+
+      @BeforeEach
+      fun setUp() {
+        court = dsl.court(
+          courtId = "SHEFMC",
+          name = "Sheffield MC",
+          description = "Sheffield Magistrates' Court",
+          active = true,
+          inactiveDate = null,
+          courtTypeCode = "MC",
+          cjitCode = "123456789",
+          areaCode = "52",
+          regionCode = "YOHUM",
+        ) {
+          address(
+            addressLine1 = "Castle Street",
+            addressLine2 = null,
+            town = "Sheffield",
+            county = "South Yorkshire",
+            postcode = "S3 8LU",
+            country = "England",
+          )
+          email(
+            emailAddress = "test.sheffield.mc@justice.gov.uk",
+          )
+          phoneNumber(
+            phoneNumber = "0114 555 5555",
+          )
+        }
+      }
+
+      @Nested
+      inner class Validation {
+        @Test
+        fun `area code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(areaCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ area code not found for agency SHEFMC")
+        }
+
+        @Test
+        fun `region code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(regionCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ region code not found for agency SHEFMC")
+        }
+
+        @Test
+        fun `court type code is not valid`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(courtTypeCode = "ZZZ"))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("ZZZ court type not found for agency SHEFMC")
+        }
+
+        @Test
+        fun `court type code missing`() {
+          val errorResponse: ErrorResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(courtTypeCode = null))
+            .exchange()
+            .expectStatus().isBadRequest.expectBodyResponse()
+
+          assertThat(errorResponse.developerMessage).isEqualTo("null court type not found for agency SHEFMC")
+        }
+      }
+
+      @Nested
+      inner class HappyPath {
+
+        @Test
+        fun `will update the core court data`() {
+          val response: LegacyAgencyResponse = webTestClient.post()
+            .uri("/sync/agency/id/{agencyId}", "SHEFMC")
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(setAuthorisation(roles = listOf("HMPPS_REGISTERS_API__SYNCHRONISATION__RW")))
+            .bodyValue(courtRequest.copy(active = false, inactiveDate = LocalDate.parse("2026-01-01")))
+            .exchange()
+            .expectStatus().isOk.expectBodyResponse()
+
+          assertThat(response.updated).isTrue
+
+          val court = courtRepository.findByCourtId("SHEFMC")
+
+          with(court) {
+            assertThat(name).isEqualTo("Sheffield MC")
+            assertThat(description).isEqualTo("Sheffield Magistrates' Court")
+            assertThat(active).isFalse
+            assertThat(inactiveDate).isEqualTo("2026-01-01")
+            assertThat(cjitCode).isEqualTo("123456789")
+            assertThat(area?.description).isEqualTo("South Yorkshire")
+            assertThat(region?.description).isEqualTo("Yorkshire & Humberside")
+            assertThat(courtType.description).isEqualTo("Magistrates Court")
+          }
         }
       }
     }
