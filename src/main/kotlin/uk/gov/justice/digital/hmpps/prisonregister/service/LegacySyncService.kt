fc21e7f69d5962b99ffdb31c2a84716afd758e9d
andymarke <andy.marke@justice.gov.uk>
2026-08-06 15:04:37 +0100

    SDIT-3937 Added Police Custody Suite to migration endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/LegacySyncService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/LegacySyncService.kt
@@ -12,13 +12,14 @@ import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyResponse
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyType
 
 @Service
-class LegacySyncService(val courtService: CourtService, val hospitalService: HospitalService, val probationOfficeService: ProbationOfficeService, val approvedPremiseService: ApprovedPremiseService) {
+class LegacySyncService(val courtService: CourtService, val hospitalService: HospitalService, val probationOfficeService: ProbationOfficeService, val approvedPremiseService: ApprovedPremiseService, val policeCustodySuiteService: PoliceCustodySuiteService) {
   fun createOrUpdateAgency(agencyId: String, agencyDto: LegacyAgencyDto): LegacyAgencyResponse = when (agencyDto.agencyType) {
     LegacyAgencyType.COURT -> courtService.createOrUpdateCourtFromLegacyData(agencyId, agencyDto)
     LegacyAgencyType.HOSPITAL -> hospitalService.createOrUpdateHospitalFromLegacyData(agencyId, agencyDto, highSecurity = false)
     LegacyAgencyType.SECURE_HOSPITAL -> hospitalService.createOrUpdateHospitalFromLegacyData(agencyId, agencyDto, highSecurity = true)
     LegacyAgencyType.PROBATION_OFFICE -> probationOfficeService.createOrUpdateProbationOfficeFromLegacyData(agencyId, agencyDto)
     LegacyAgencyType.APPROVED_PREMISE -> approvedPremiseService.createOrUpdateApprovedPremiseFromLegacyData(agencyId, agencyDto)
+    LegacyAgencyType.POLICE_CUSTODY_SUITE -> policeCustodySuiteService.createOrUpdatePoliceCustodySuiteFromLegacyData(agencyId, agencyDto)
     else -> throw IllegalArgumentException("Unsupported agency type: ${agencyDto.agencyType}")
   }
 }
