877ff3235a06e06e0b41dba2608c5a84c62bfe36
andymarke <andy.marke@justice.gov.uk>
2026-08-06 10:30:29 +0100

    SDIT-3937 Added Probation sync

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/model/ProbationOfficeRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/model/ProbationOfficeRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProbationOfficeRepository : JpaRepository<ProbationOffice, String>
+interface ProbationOfficeRepository : JpaRepository<ProbationOffice, String> {
+  fun findByProbationOfficeId(probationOfficeId: String): ProbationOffice
+}
