86f144743df1d253b7daa787149001a8d92370de
andymarke <andy.marke@justice.gov.uk>
2026-08-25 10:42:38 +0100

    SDIT-4149 Add AccessibleAccess to Court
    Signed-off-by: andymarke <andy.marke@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/model/Court.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/model/Court.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.prisonregister.model
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
@@ -24,6 +26,7 @@ data class Court(
   var active: Boolean,
   var inactiveDate: LocalDate?,
   var cjitCode: String?,
+  @Enumerated(EnumType.STRING)
   var accessibleAccess: AccessibleAccess?,
 
   @ManyToOne(fetch = FetchType.LAZY)
