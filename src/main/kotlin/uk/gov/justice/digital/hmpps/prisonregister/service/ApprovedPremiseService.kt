c08f5d81f412e5b0cbeeaa00b6fb74da12587207
lbennett-moj <lucy.bennett1@justice.gov.uk>
2026-09-15 11:40:56 +0100

    sdit_4230 add all endpoints required for UI usage to ApprovedPremises

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/ApprovedPremiseService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonregister/service/ApprovedPremiseService.kt
@@ -5,14 +5,21 @@ import jakarta.validation.ValidationException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.prisonregister.exceptions.EmailAddressAlreadyExistsException
+import uk.gov.justice.digital.hmpps.prisonregister.exceptions.PhoneNumberAlreadyExistsException
 import uk.gov.justice.digital.hmpps.prisonregister.model.AccessibleAccess
+import uk.gov.justice.digital.hmpps.prisonregister.model.AgencyAddress
 import uk.gov.justice.digital.hmpps.prisonregister.model.ApprovedPremise
 import uk.gov.justice.digital.hmpps.prisonregister.model.ApprovedPremiseRepository
 import uk.gov.justice.digital.hmpps.prisonregister.model.AreaRepository
+import uk.gov.justice.digital.hmpps.prisonregister.model.EmailAddress
+import uk.gov.justice.digital.hmpps.prisonregister.model.EmailAddressRepository
 import uk.gov.justice.digital.hmpps.prisonregister.model.LocalAuthorityRepository
 import uk.gov.justice.digital.hmpps.prisonregister.model.PayrollRegionRepository
+import uk.gov.justice.digital.hmpps.prisonregister.model.PhoneNumber
 import uk.gov.justice.digital.hmpps.prisonregister.model.RegionRepository
 import uk.gov.justice.digital.hmpps.prisonregister.resource.ApprovedPremiseDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.CreateApprovedPremiseDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAccessibleAccess
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyAddressDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyDto
@@ -20,6 +27,10 @@ import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyEmailDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyPhoneDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyResponse
 import uk.gov.justice.digital.hmpps.prisonregister.resource.LegacyAgencyType
+import uk.gov.justice.digital.hmpps.prisonregister.resource.UpdateAddressDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.UpdateApprovedPremiseDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.UpdateEmailAddressDto
+import uk.gov.justice.digital.hmpps.prisonregister.resource.UpdatePhoneNumberDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.dto.AgencyAddressDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.dto.AgencyEmailDto
 import uk.gov.justice.digital.hmpps.prisonregister.resource.dto.AgencyPhoneDto
@@ -33,6 +44,7 @@ class ApprovedPremiseService(
   private val regionRepository: RegionRepository,
   private val payrollRegionRepository: PayrollRegionRepository,
   private val localAuthorityRepository: LocalAuthorityRepository,
+  private val emailAddressRepository: EmailAddressRepository,
 ) {
   fun deleteAll() {
     approvedPremiseRepository.deleteAll()
@@ -40,46 +52,223 @@ class ApprovedPremiseService(
 
   fun getAllIds(): List<String> = approvedPremiseRepository.findAll().map { it.approvedPremiseId }
 
-  fun findById(approvedPremiseId: String): ApprovedPremiseDto = approvedPremiseRepository.findByIdOrNull(approvedPremiseId)?.let {
-    ApprovedPremiseDto(
-      approvedPremiseId = it.approvedPremiseId,
-      approvedPremiseName = it.name,
-      description = it.description,
-      contact = it.contact,
-      active = it.active,
-      accessibleAccess = it.accessibleAccess?.name,
-      inactiveDate = it.inactiveDate,
-      cjitCode = it.cjitCode,
-      area = it.area?.let { area -> CodeDescription(area.code, area.description) },
-      region = it.region?.let { region -> CodeDescription(region.code, region.description) },
-      geographicalArea = it.geographicalArea?.let { area -> CodeDescription(area.code, area.description) },
-      localAuthority = it.localAuthority?.let { localAuthority -> CodeDescription(localAuthority.code, localAuthority.description) },
-      payrollRegion = it.payrollRegion?.let { pr -> CodeDescription(pr.code, pr.description) },
-      addresses = it.addresses.map { address ->
-        AgencyAddressDto(
-          id = address.id,
-          addressLine1 = address.addressLine1,
-          addressLine2 = address.addressLine2,
-          town = address.town,
-          county = address.county,
-          postcode = address.postcode,
-          country = address.country,
-        )
-      },
-      emailAddresses = it.emailAddresses.map { emailAddress ->
-        AgencyEmailDto(
-          id = emailAddress.id,
-          address = emailAddress.value,
-        )
-      },
-      phoneNumbers = it.phoneNumbers.map { phoneNumber ->
-        AgencyPhoneDto(
-          id = phoneNumber.id,
-          number = phoneNumber.value,
-        )
-      },
+  fun getAll(): List<ApprovedPremiseDto> = approvedPremiseRepository.findAll().map { it.toApprovedPremiseDto() }
+
+  fun findById(approvedPremiseId: String): ApprovedPremiseDto = approvedPremiseRepository.findByIdOrNull(approvedPremiseId)?.toApprovedPremiseDto()
+    ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+
+  fun createApprovedPremise(createApprovedPremiseDto: CreateApprovedPremiseDto): ApprovedPremiseDto {
+    if (approvedPremiseRepository.existsById(createApprovedPremiseDto.approvedPremiseId)) {
+      throw ValidationException("Approved premise ${createApprovedPremiseDto.approvedPremiseId} already exists")
+    }
+
+    // todo add validation below when correct constraints known
+    val approvedPremise = createApprovedPremiseDto.toApprovedPremise()
+    approvedPremise.addresses += createApprovedPremiseDto.addresses.map { it.toAgencyAddress() }
+    approvedPremise.emailAddresses += createApprovedPremiseDto.emailAddresses.map { EmailAddress(it.address) }
+    approvedPremise.phoneNumbers += createApprovedPremiseDto.phoneNumbers.map { PhoneNumber(it.number) }
+
+    return approvedPremiseRepository.saveAndFlush(approvedPremise).toApprovedPremiseDto()
+  }
+
+  fun updateApprovedPremise(approvedPremiseId: String, updateApprovedPremiseDto: UpdateApprovedPremiseDto): ApprovedPremiseDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    approvedPremise.update(updateApprovedPremiseDto)
+    return approvedPremise.toApprovedPremiseDto()
+  }
+
+  fun deleteApprovedPremise(approvedPremiseId: String) {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    approvedPremiseRepository.delete(approvedPremise)
+  }
+
+  fun createApprovedPremiseAddress(approvedPremiseId: String, updateAddressDto: UpdateAddressDto): AgencyAddressDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+
+    val address = updateAddressDto.toAgencyAddress()
+    approvedPremise.addresses += address
+    approvedPremiseRepository.flush()
+
+    return AgencyAddressDto(
+      id = address.id,
+      addressLine1 = address.addressLine1,
+      addressLine2 = address.addressLine2,
+      town = address.town,
+      county = address.county,
+      postcode = address.postcode,
+      country = address.country,
     )
-  } ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+  }
+
+  fun updateApprovedPremiseAddress(approvedPremiseId: String, addressId: Long, updateAddressDto: UpdateAddressDto): AgencyAddressDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val address = approvedPremise.addresses.find { it.id == addressId } ?: throw EntityNotFoundException("Address $addressId not found for approved premise $approvedPremiseId")
+
+    with(updateAddressDto) {
+      address.addressLine1 = addressLine1
+      address.addressLine2 = addressLine2
+      address.town = town
+      address.county = county
+      address.postcode = postcode
+      address.country = country
+    }
+
+    return AgencyAddressDto(
+      id = address.id,
+      addressLine1 = address.addressLine1,
+      addressLine2 = address.addressLine2,
+      town = address.town,
+      county = address.county,
+      postcode = address.postcode,
+      country = address.country,
+    )
+  }
+
+  fun deleteApprovedPremiseAddress(approvedPremiseId: String, addressId: Long): AgencyAddressDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val address = approvedPremise.addresses.find { it.id == addressId } ?: throw EntityNotFoundException("Address $addressId not found for approved premise $approvedPremiseId")
+
+    approvedPremise.addresses.remove(address)
+
+    // returned for audit payload
+    return AgencyAddressDto(
+      id = address.id,
+      addressLine1 = address.addressLine1,
+      addressLine2 = address.addressLine2,
+      town = address.town,
+      county = address.county,
+      postcode = address.postcode,
+      country = address.country,
+    )
+  }
+
+  fun createApprovedPremisePhoneNumber(approvedPremiseId: String, updatePhoneNumberDto: UpdatePhoneNumberDto): AgencyPhoneDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+
+    // phone number must be unique within the approved premise
+    if (approvedPremise.phoneNumbers.any { it.value == updatePhoneNumberDto.number }) {
+      throw PhoneNumberAlreadyExistsException(updatePhoneNumberDto.number)
+    }
+
+    val phoneNumber = PhoneNumber(updatePhoneNumberDto.number)
+    approvedPremise.phoneNumbers += phoneNumber
+    approvedPremiseRepository.flush()
+
+    return AgencyPhoneDto(
+      id = phoneNumber.id,
+      number = phoneNumber.value,
+    )
+  }
+
+  fun updateApprovedPremisePhoneNumber(approvedPremiseId: String, phoneNumberId: Long, updatePhoneNumberDto: UpdatePhoneNumberDto): AgencyPhoneDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val phoneNumber = approvedPremise.phoneNumbers.find { it.id == phoneNumberId } ?: throw EntityNotFoundException("Phone number $phoneNumberId not found for approved premise $approvedPremiseId")
+
+    // phone number must be unique within the approved premise
+    if (approvedPremise.phoneNumbers.any { it.id != phoneNumberId && it.value == updatePhoneNumberDto.number }) {
+      throw PhoneNumberAlreadyExistsException(updatePhoneNumberDto.number)
+    }
+
+    phoneNumber.value = updatePhoneNumberDto.number
+
+    return AgencyPhoneDto(
+      id = phoneNumber.id,
+      number = phoneNumber.value,
+    )
+  }
+
+  fun deleteApprovedPremisePhoneNumber(approvedPremiseId: String, phoneNumberId: Long): AgencyPhoneDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val phoneNumber = approvedPremise.phoneNumbers.find { it.id == phoneNumberId } ?: throw EntityNotFoundException("Phone number $phoneNumberId not found for approved premise $approvedPremiseId")
+
+    approvedPremise.phoneNumbers.remove(phoneNumber)
+
+    return AgencyPhoneDto(
+      id = phoneNumber.id,
+      number = phoneNumber.value,
+    )
+  }
+
+  fun createApprovedPremiseEmailAddress(approvedPremiseId: String, updateEmailAddressDto: UpdateEmailAddressDto): AgencyEmailDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+
+    // email address is unique across all establishments
+    if (emailAddressRepository.getByValue(updateEmailAddressDto.address) != null) {
+      throw EmailAddressAlreadyExistsException(updateEmailAddressDto.address)
+    }
+
+    val emailAddress = EmailAddress(updateEmailAddressDto.address)
+    approvedPremise.emailAddresses += emailAddress
+    approvedPremiseRepository.flush()
+
+    return AgencyEmailDto(
+      id = emailAddress.id,
+      address = emailAddress.value,
+    )
+  }
+
+  fun updateApprovedPremiseEmailAddress(approvedPremiseId: String, emailAddressId: Long, updateEmailAddressDto: UpdateEmailAddressDto): AgencyEmailDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val emailAddress = approvedPremise.emailAddresses.find { it.id == emailAddressId } ?: throw EntityNotFoundException("Email address $emailAddressId not found for approved premise $approvedPremiseId")
+
+    emailAddress.value = updateEmailAddressDto.address
+
+    return AgencyEmailDto(
+      id = emailAddress.id,
+      address = emailAddress.value,
+    )
+  }
+
+  fun deleteApprovedPremiseEmailAddress(approvedPremiseId: String, emailAddressId: Long): AgencyEmailDto {
+    val approvedPremise = approvedPremiseRepository.findByIdOrNull(approvedPremiseId) ?: throw EntityNotFoundException("Approved premise $approvedPremiseId not found")
+    val emailAddress = approvedPremise.emailAddresses.find { it.id == emailAddressId } ?: throw EntityNotFoundException("Email address $emailAddressId not found for approved premise $approvedPremiseId")
+
+    approvedPremise.emailAddresses.remove(emailAddress)
+
+    return AgencyEmailDto(
+      id = emailAddress.id,
+      address = emailAddress.value,
+    )
+  }
+
+  private fun ApprovedPremise.toApprovedPremiseDto() = ApprovedPremiseDto(
+    approvedPremiseId = this.approvedPremiseId,
+    approvedPremiseName = this.name,
+    description = this.description,
+    contact = this.contact,
+    active = this.active,
+    accessibleAccess = this.accessibleAccess?.name,
+    inactiveDate = this.inactiveDate,
+    cjitCode = this.cjitCode,
+    area = this.area?.let { area -> CodeDescription(area.code, area.description) },
+    region = this.region?.let { region -> CodeDescription(region.code, region.description) },
+    geographicalArea = this.geographicalArea?.let { area -> CodeDescription(area.code, area.description) },
+    localAuthority = this.localAuthority?.let { localAuthority -> CodeDescription(localAuthority.code, localAuthority.description) },
+    payrollRegion = this.payrollRegion?.let { pr -> CodeDescription(pr.code, pr.description) },
+    addresses = this.addresses.map { address ->
+      AgencyAddressDto(
+        id = address.id,
+        addressLine1 = address.addressLine1,
+        addressLine2 = address.addressLine2,
+        town = address.town,
+        county = address.county,
+        postcode = address.postcode,
+        country = address.country,
+      )
+    },
+    emailAddresses = this.emailAddresses.map { emailAddress ->
+      AgencyEmailDto(
+        id = emailAddress.id,
+        address = emailAddress.value,
+      )
+    },
+    phoneNumbers = this.phoneNumbers.map { phoneNumber ->
+      AgencyPhoneDto(
+        id = phoneNumber.id,
+        number = phoneNumber.value,
+      )
+    },
+  )
 
   fun tryFindById(agencyId: String): LegacyAgencyDto? = approvedPremiseRepository.findByIdOrNull(agencyId)?.let { ap ->
     LegacyAgencyDto(
@@ -155,4 +344,45 @@ class ApprovedPremiseService(
     this.payrollRegion = agencyDto.payrollRegionCode?.let { payrollRegionRepository.findByIdOrNull(it) ?: throw ValidationException("$it payroll region code not found for agency $approvedPremiseId") }
     this.localAuthority = agencyDto.localAuthorityCode?.let { localAuthorityRepository.findByIdOrNull(it) ?: throw ValidationException("$it local authority code not found for agency $approvedPremiseId") }
   }
+
+  private fun ApprovedPremise.update(updateApprovedPremiseDto: UpdateApprovedPremiseDto) {
+    this.name = updateApprovedPremiseDto.approvedPremiseName
+    this.description = updateApprovedPremiseDto.description
+    this.contact = updateApprovedPremiseDto.contact
+    this.active = updateApprovedPremiseDto.active
+    this.accessibleAccess = updateApprovedPremiseDto.accessibleAccess
+    this.inactiveDate = updateApprovedPremiseDto.inactiveDate
+    this.cjitCode = updateApprovedPremiseDto.cjitCode
+    this.area = updateApprovedPremiseDto.areaCode?.let { areaRepository.findByIdOrNull(it) ?: throw ValidationException("$it area code not found for approved premise $approvedPremiseId") }
+    this.region = updateApprovedPremiseDto.regionCode?.let { regionRepository.findByIdOrNull(it) ?: throw ValidationException("$it region code not found for approved premise $approvedPremiseId") }
+    this.geographicalArea = updateApprovedPremiseDto.geographicalAreaCode?.let { areaRepository.findByIdOrNull(it) ?: throw ValidationException("$it geographical area code not found for approved premise $approvedPremiseId") }
+    this.payrollRegion = updateApprovedPremiseDto.payrollRegionCode?.let { payrollRegionRepository.findByIdOrNull(it) ?: throw ValidationException("$it payroll region code not found for approved premise $approvedPremiseId") }
+    this.localAuthority = updateApprovedPremiseDto.localAuthorityCode?.let { localAuthorityRepository.findByIdOrNull(it) ?: throw ValidationException("$it local authority code not found for approved premise $approvedPremiseId") }
+  }
+
+  private fun CreateApprovedPremiseDto.toApprovedPremise() = ApprovedPremise(
+    approvedPremiseId = this.approvedPremiseId,
+    name = this.approvedPremiseName,
+    description = this.description,
+    contact = this.contact,
+    active = this.active,
+    accessibleAccess = this.accessibleAccess,
+    inactiveDate = this.inactiveDate,
+    cjitCode = this.cjitCode,
+    area = this.areaCode?.let { areaRepository.findByIdOrNull(it) ?: throw ValidationException("$it area code not found for approved premise $approvedPremiseId") },
+    region = this.regionCode?.let { regionRepository.findByIdOrNull(it) ?: throw ValidationException("$it region code not found for approved premise $approvedPremiseId") },
+    geographicalArea = this.geographicalAreaCode?.let { areaRepository.findByIdOrNull(it) ?: throw ValidationException("$it geographical area code not found for approved premise $approvedPremiseId") },
+    payrollRegion = this.payrollRegionCode?.let { payrollRegionRepository.findByIdOrNull(it) ?: throw ValidationException("$it payroll region code not found for approved premise $approvedPremiseId") },
+    localAuthority = this.localAuthorityCode?.let { localAuthorityRepository.findByIdOrNull(it) ?: throw ValidationException("$it local authority code not found for approved premise $approvedPremiseId") },
+    new = true,
+  )
+
+  private fun UpdateAddressDto.toAgencyAddress() = AgencyAddress(
+    addressLine1 = this.addressLine1,
+    addressLine2 = this.addressLine2,
+    town = this.town,
+    county = this.county,
+    postcode = this.postcode,
+    country = this.country,
+  )
 }
